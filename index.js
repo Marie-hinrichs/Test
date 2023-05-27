a0c4f83bb41f63d783cc7a0681185125d13be439
Marie-hinrichs <hinimini209@gmail.com>
2023-05-27 17:21:28 +0200

    lel

--- index.js
+++ index.js
@@ -3,17 +3,14 @@ const http = require("http");
 const socketIO = require("socket.io");
 const app = express();
 const server = http.createServer(app);
-
-// Statische Dateien aus dem "public" Verzeichnis bereitstellen
-app.use(express.static("public"));
-
 const io = socketIO(server);
-
-// Die Spiellogik und Socket.IO-Verbindungen in "app.js" behandeln
-require("./app")(io);
 
 const port = process.env.PORT || 3000;
 
+// Importiere die app.js-Datei
+const game = require("./app.js");
+game(io);
+
 server.listen(port, () => {
-  console.log(`Server gestartet auf Port ${port}`);
+  console.log(`Server läuft auf Port ${port}`);
 });
--- public/app.js
+++ public/app.js
@@ -5,19 +5,16 @@ module.exports = (io) => {
   io.on("connection", (socket) => {
     console.log("Neue Verbindung hergestellt:", socket.id);
 
-    // Neuen Spieler hinzufügen
     const player = {
       id: socket.id,
-      x: Math.random() * 800, // Zufällige Startposition X
-      y: Math.random() * 600, // Zufällige Startposition Y
-      color: "#" + ((Math.random() * 0xffffff) << 0).toString(16), // Zufällige Farbe
+      x: Math.random() * 800,
+      y: Math.random() * 600,
+      color: "#" + ((Math.random() * 0xffffff) << 0).toString(16),
     };
     players.push(player);
 
-    // Spielerliste an den verbundenen Client senden
     socket.emit("init", { players, tagger });
 
-    // Wenn ein Spieler sich bewegt, die Position aktualisieren und an alle Clients senden
     socket.on("move", (movement) => {
       const { x, y } = movement;
       player.x = x;
@@ -25,7 +22,6 @@ module.exports = (io) => {
       io.emit("update", player);
     });
 
-    // Wenn ein Spieler das Spiel verlässt, ihn aus der Spielerliste entfernen und an alle Clients senden
     socket.on("disconnect", () => {
       const index = players.findIndex((p) => p.id === socket.id);
       if (index !== -1) {
@@ -37,7 +33,6 @@ module.exports = (io) => {
       }
     });
 
-    // Wenn das Spiel gestartet wird und mindestens zwei Spieler vorhanden sind, einen Fänger auswählen
     if (players.length >= 2 && !tagger) {
       tagger = players[Math.floor(Math.random() * players.length)].id;
       io.emit("taggerSelected", tagger);
